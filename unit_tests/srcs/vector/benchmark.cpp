e8502c4881612df7f2e7794446ce74ab375d50e3
Maxime Riaud (at 42) <mriaud@student.42.fr>
2022-11-23 17:59:47 +0100

    update vector benchmark

--- unit_tests/srcs/vector/benchmark.cpp
+++ unit_tests/srcs/vector/benchmark.cpp
@@ -6,6 +6,8 @@
 #define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
 #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
+#define BENCHMARK_SIZE 1000000
+
 
 TEST_CASE("Vector benchmarck", "[!hide][vector][benchmark]") {
 
@@ -32,6 +34,24 @@ TEST_CASE("Vector benchmarck", "[!hide][vector][benchmark]") {
 			vec.insert(vec.begin() + vec.size() / 2, sample_vec.begin(), sample_vec.end());
 
 		std::cout << "\n" << BOLDGREEN << vec.size() << " elements were written in std::vector using push_back() and insert()" << RESET << std::endl;
+	}
+
+	StdVec	std_vec;
+	for(size_t i = 0; i < BENCHMARK_SIZE; i++)
+		std_vec.push_back(Custom::mocking_value<ValueType>());
+	Vec		vec(std_vec.begin(), std_vec.end());
+
+	SECTION("ft::vector erase") {
+		while(vec.size() > 10)
+			vec.erase(vec.begin() + vec.size() / 3, vec.begin() + vec.size() / 2);
+
+		std::cout << "\n\n" << BOLDBLUE << BENCHMARK_SIZE - vec.size() << " elements were erased in ft::vector using erase()" << RESET << std::endl;
+	}
+	SECTION("std::vector erase") {
+		while(std_vec.size() > 10)
+			std_vec.erase(std_vec.begin() + std_vec.size() / 3, std_vec.begin() + std_vec.size() / 2);
+
+		std::cout << "\n" << BOLDGREEN << BENCHMARK_SIZE - std_vec.size() << " elements were erased in std::vector using erase()" << RESET << std::endl;
 	}
 }
 
